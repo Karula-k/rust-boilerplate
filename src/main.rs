89b34adb41315a6aa310d17604753109335f8f14
Karula-k <d.fikriirsyad@gmail.com>
2026-07-26 12:26:40 +0700

    feat: update the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+// will remove when implementation cornucopia
+#![allow(dead_code)]
+
+mod config;
+
+use std::sync::Arc;
+
 use axum::{Router, routing::get};
+use tokio_postgres::{Client, NoTls};
+
+use crate::config::Config;
+
+struct AppState {
+    db: Client,
+}
 
 #[tokio::main]
 async fn main() {
-      let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-      let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+      let config = Config::from_env();
+     let (client, connection) = tokio_postgres::connect(&config.database_url, NoTls).await.unwrap();
+        tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+     let app_state = Arc::new(AppState { db: client });
+      let app = Router::new().route("/", get(|| async { "Hello, World!" })).with_state(app_state);
+      let listener = tokio::net::TcpListener::bind(config.address()).await.unwrap();
       axum::serve(listener, app).await.unwrap();
 }
