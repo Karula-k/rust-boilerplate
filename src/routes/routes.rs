3734af30e9f51803fe8161afea91ad384de86606
Karula-k <d.fikriirsyad@gmail.com>
2026-08-02 13:59:10 +0700

    feat: seperate protected route and visible route

--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -2,13 +2,22 @@ use std::sync::Arc;
 
 use axum::{Router, routing::{get, post}};
 
-use crate::{AppState, handlers::{auth_handler, users_handler}};
+use crate::{AppState, handlers::{auth_handler, users_handler}, middleware::middleware::auth_middleware};
 
 
 
-pub fn routes() -> Router<Arc<AppState>> {
-    Router::new()
-    .route("/register", post(auth_handler::register))
-   .route("/login", post(auth_handler::login))
-   .route("/users", get(users_handler::list_users))
+pub fn routes(state: &Arc<AppState>) -> Router<Arc<AppState>> {
+    let public_routes = Router::new()
+        .route("/register", post(auth_handler::register))
+        .route("/login", post(auth_handler::login));
+
+    let protected_routes = Router::new()
+        .route("/users", get(users_handler::list_users))
+        .layer(axum::middleware::from_fn_with_state(
+            state.clone(),
+            auth_middleware,
+        ));
+
+    public_routes
+        .merge(protected_routes)
 }
