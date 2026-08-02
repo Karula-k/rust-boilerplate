ef1f541f445f945fe6ad50bab9390e7fbb2d5192
Karula-k <d.fikriirsyad@gmail.com>
2026-08-02 13:19:32 +0700

    feat: update config

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -6,6 +6,7 @@ pub struct Config{
     pub database_url: String,
     pub host:String,
     pub port:String,
+    pub secret: String,
 }
 
 impl Config {
@@ -15,6 +16,8 @@ impl Config {
         database_url: env::var("DATABASE_URL").expect("Check Database env"),
         host:env::var("HOST").unwrap_or_else(|_|"0.0.0.0".to_string()),
         port:env::var("PORT").unwrap_or_else(|_|"3000".to_string()).parse().expect("Port must be valid u16"), 
+        secret:env::var("JWT_SECRET").unwrap_or_else(|_|"SECRET".to_string()),
+
     }
     }
     pub  fn address(&self)->String{
