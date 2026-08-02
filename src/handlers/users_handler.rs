840df4fe4b372fa1f2cb65fb5d8644aa46b4e513
Karula-k <d.fikriirsyad@gmail.com>
2026-08-02 13:20:47 +0700

    feat: fix bug offset start from 5

--- src/handlers/users_handler.rs
+++ src/handlers/users_handler.rs
@@ -9,7 +9,7 @@ use crate::{AppState, utils::error::AppError, utils::response::{ApiResponse}};
 pub async fn list_users(State( states): State<Arc<AppState>>,)->Result<ApiResponse<Vec<users::ListUsers>>,AppError>{
   
     let rows: Vec<users::ListUsers> = users::list_users()
-        .bind(&states.db_pool, &10, &5)
+        .bind(&states.db_pool, &10, &0)
         .all().await?;
 
  Ok(ApiResponse::JsonData(rows))
